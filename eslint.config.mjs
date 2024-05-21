e9d087ef3f52118e71067eb693d7b486e0750b86
Habib <h.hourany@gmail.com>
2024-05-21 14:42:38 +0200

    install playwright and fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,21 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import reactApp from "eslint-config-react-app";
+import reactAppJest from "eslint-config-react-app/jest";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  ...reactApp,
+  ...reactAppJest,
+  {
+    // Added as it was generating unnecessary errors for playwright tests
+    files: ["**/*.spec.ts"],
+    rules: {
+      "testing-library/prefer-screen-queries": "off",
+    },
+  },
 ];
