b1c0d3f0d5fee8bf99c96b50d2b0e521d612595e
Habib <h.hourany@gmail.com>
2024-05-22 10:07:58 +0200

    move tests to e2e/tests folder

--- e2e/tests/rectangle.spec.ts
+++ e2e/tests/rectangle.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "./fixtures/fixture";
+import { test, expect } from "../fixtures/fixture";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
