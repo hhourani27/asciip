92828e343cc1438c5a081b73f203b10802828de8
Habib <h.hourany@gmail.com>
2024-05-22 07:59:27 +0200

    setup playwright and add first tests

---
+++ e2e/pom/Canvas.ts
@@ -0,0 +1,59 @@
+import type { Page, Locator } from "@playwright/test";
+import { CELL_WIDTH, CELL_HEIGHT } from "../../src/components/canvas/draw";
+
+export class Canvas {
+  private readonly page: Page;
+  public readonly mouse: CanvasMouse;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.mouse = new CanvasMouse(page);
+  }
+
+  locator(): Locator {
+    return this.page.locator("#canvas-container");
+  }
+}
+
+class CanvasMouse {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async move(r: number, c: number) {
+    await this.page.mouse.move(...(await this.coords(r, c)), { steps: 30 });
+  }
+
+  async down() {
+    await this.page.mouse.down();
+  }
+
+  async up() {
+    await this.page.mouse.up();
+  }
+
+  async leave() {
+    this.page.mouse.move(0, 0);
+  }
+
+  private async coords(r: number, c: number): Promise<[number, number]> {
+    // Select the canvas element
+    const canvas = await this.page.$("canvas");
+    if (!canvas) {
+      throw new Error("Canvas element not found");
+    }
+
+    // Get the bounding box of the canvas
+    const canvasBB = await canvas.boundingBox();
+    if (!canvasBB) {
+      throw new Error("Bounding box not found");
+    }
+
+    const canvasX = c * CELL_WIDTH + 0.5 * CELL_WIDTH;
+    const canvasY = r * CELL_HEIGHT + 0.5 * CELL_HEIGHT;
+
+    return [canvasBB.x + canvasX, canvasBB.y + canvasY];
+  }
+}
---
+++ e2e/rectangle.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "./fixtures/fixture";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+});
+
+test.afterEach(async ({ page }) => {
+  await page.evaluate(() => {
+    localStorage.clear();
+  });
+});
+
+test.describe("Create rectangle", () => {
+  test("Create a 10x10 Rectangle by starting from the top left", async ({
+    page,
+    canvas,
+  }) => {
+    await page.getByRole("button", { name: "Create Rectangle" }).click();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.down();
+    await canvas.mouse.move(9, 9);
+    await canvas.mouse.up();
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Create a 10x10 Rectangle by starting from the bottom right", async ({
+    page,
+    canvas,
+  }) => {
+    await page.getByRole("button", { name: "Create Rectangle" }).click();
+
+    await canvas.mouse.move(9, 9);
+    await canvas.mouse.down();
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.up();
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+});
