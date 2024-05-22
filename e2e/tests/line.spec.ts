f0b1271bf1ee30979813b76db72a6f0ec9902577
Habib <h.hourany@gmail.com>
2024-05-22 10:40:19 +0200

    Add tests : Resize horizontal Line

--- e2e/tests/line.spec.ts
+++ e2e/tests/line.spec.ts
@@ -70,3 +70,131 @@ test.describe("Create Line", () => {
     await expect(canvas.locator()).toHaveScreenshot();
   });
 });
+
+test.describe("Resize horizontal Line", () => {
+  test.beforeEach(async ({ page, canvas }) => {
+    await page.getByRole("button", { name: "Create Simple Line" }).click();
+
+    await canvas.mouse.move(5, 5);
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.up();
+    await canvas.mouse.leave();
+
+    await page.getByRole("button", { name: "Select tool" }).click();
+  });
+
+  test("Extend horizontal line by dragging the end point", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 15);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Shrink horizontal line by dragging the end point", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 7);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Extend horizontal line by dragging the start point", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 5);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 0);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Shrink horizontal line by dragging the start point", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 7);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Turn horizontal line 180 degres by dragging the end point", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 0);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+
+  test("Cannot shrink horizontal line to a zero-length line", async ({
+    page,
+    canvas,
+  }) => {
+    await canvas.mouse.move(5, 10);
+    await canvas.mouse.click();
+
+    await canvas.mouse.down();
+    await canvas.mouse.move(5, 5);
+    await canvas.mouse.up();
+
+    await canvas.mouse.move(0, 0);
+    await canvas.mouse.click();
+
+    await canvas.mouse.leave();
+
+    await expect(canvas.locator()).toHaveScreenshot();
+  });
+});
